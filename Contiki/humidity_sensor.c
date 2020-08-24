a20883ed0ebe2c6aa21d70ce20897645915ca465
gerry94 <g.alvaro@studenti.unipi.it>
2020-08-24 16:48:11 +0200

    First Contiki Implementation

---
+++ Contiki/humidity_sensor.c
@@ -0,0 +1,80 @@
+#include "humidity_sensor.h"
+
+static coap_message_type_t result = COAP_TYPE_RST;
+int TIME; //globale perchè contiki lo resetta
+const int VAR_RANGE = 5; //range di variazione dell'umidità (in percentuale)
+
+PROCESS(humidity_sensor, "Humidity Sensor");
+AUTOSTART_PROCESSES(&humidity_sensor);
+
+static void response_handler(coap_message_t *response){
+
+    if (response == NULL)
+        return;
+        
+    LOG_DBG("Response %i\n", response->type);
+    result = response->type;
+}
+
+PROCESS_THREAD(humidity_sensor, ev, data) {
+
+    static coap_endpoint_t server_ep;
+    static coap_message_t request[1];
+
+    PROCESS_BEGIN();
+
+    LOG_INFO("Starting humidity sensor \n");
+
+    coap_activate_resource(&res_humidity, "sensors/humidity");
+    coap_activate_resource(&res_irrigator, "actuators/irrigator");
+	
+	//initialize the timer
+	TIME = ((random_rand() % 10)+1 );
+	static struct etimer timer;
+	etimer_set(&timer, TIME*CLOCK_SECOND);
+
+    coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+
+    do {
+
+        coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+        coap_set_header_uri_path(request, (const char *) &SERVER_REGISTRATION);
+        COAP_BLOCKING_REQUEST(&server_ep, request, response_handler);
+        
+    } while (result == COAP_TYPE_RST);
+
+    while (1) {
+
+        PROCESS_YIELD_UNTIL(etimer_expired(&timer));
+       
+        if (etimer_expired(&timer)){
+
+            if (humidity == -1)
+                humidity = 30; //((float)rand() / RAND_MAX) * (val_max - val_min) + val_min 
+			
+			//randomly choose if there has been a variation
+			if((TIME % 2) == 0)
+			{
+				int var = (random_rand() % VAR_RANGE);
+				if(var > 0)
+				{ 		
+					//decide wether it is an increase or decrease of humidity (50% chance)
+					if(random_rand() % 2 == 0) var = var*(-1);
+
+					humidity += var;
+					printf("Humidity variation registered, new humidity: %d \n", humidity);
+
+					//trigger the event to notify observers
+					res_humidity.trigger();
+				}
+				//else if(debug) printf("DEBUG: variazione = 0\n"); 
+			}
+			//else if(debug) printf("...\n");
+		}
+		   //reset random timer
+		    TIME = ((random_rand() % 10) +1);
+		    etimer_set(&timer, TIME*CLOCK_SECOND);
+    }
+
+    PROCESS_END();
+}
---
+++ Contiki/res_humidity.c
@@ -0,0 +1,52 @@
+#include "contiki.h"
+#include "coap-engine.h"
+#include "sys/log.h"
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LOG_MODULE "Humidity sensor"
+#define LOG_LEVEL LOG_LEVEL_INFO
+#define MAX_AGE 60
+
+static void res_get_handler(coap_message_t *, coap_message_t *, uint8_t *, uint16_t, int32_t *);
+static void res_event_handler();
+
+//extern uint8_t humidifier_status;
+//extern uint8_t humidifier_value;
+
+float humidity = -1;
+float last_humidity = -1;
+
+EVENT_RESOURCE(
+    res_humidity,
+    "title=\"Humidity sensor\"; GET; rt=\"Sensor\"; obs\n",
+    res_get_handler, NULL, NULL, NULL, res_event_handler);
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
+
+    unsigned int accept = APPLICATION_JSON;
+    coap_get_header_accept(request, &accept);
+
+    if (accept == APPLICATION_JSON) {
+        coap_set_header_content_format(response, APPLICATION_JSON);
+        snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"humidity\":%.1f}", humidity);
+        coap_set_payload(response, buffer, strlen((char *)buffer));
+    } else {
+        coap_set_status_code(response, NOT_ACCEPTABLE_4_06);
+        const char *msg = "Supporting content-type application/json";
+        coap_set_payload(response, msg, strlen(msg));
+    }
+
+    coap_set_header_max_age(response, MAX_AGE);
+}
+
+static void res_event_handler(){
+
+    if (last_humidity != humidity){
+        last_humidity = humidity;
+        coap_notify_observers(&res_humidity);
+    }
+}
